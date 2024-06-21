f86cee490ad87d2503004ff53af15d8f33a9be73
ManuVillalba95 <manuunzain@gmail.com>
2024-06-21 00:47:21 -0300

    Funciones Nuevas

--- CRUDSKU/CRUDSKU/Controllers/SKUController.cs
+++ CRUDSKU/CRUDSKU/Controllers/SKUController.cs
@@ -34,5 +34,100 @@ namespace CRUDSKU.Controllers
 
             return View(skus);
         }
-    }
+
+		
+		public IActionResult Create()
+		{
+			return View();
+		}
+
+		
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public IActionResult Create([Bind("Codigo,Nombre,Descripcion")] SKU sku)
+		{
+			if (ModelState.IsValid)
+			{
+				DA_SKU daSKU = new DA_SKU(_configuration.GetConnectionString("conexion"));
+				daSKU.Insertar(sku);
+
+				return RedirectToAction(nameof(Index));
+			}
+			return View(sku);
+		}
+		public IActionResult Edit(int id)
+		{
+			DA_SKU daSKU = new(_configuration.GetConnectionString("conexion"));
+			DataTable dt = daSKU.Obtener(id);
+			if (dt.Rows.Count == 0)
+			{
+				return NotFound();
+			}
+
+			DataRow dr = dt.Rows[0];
+			SKU sku = new SKU
+			{
+				Id = (int)dr["IdSKU"],
+				Codigo = int.Parse(dr["CodProducto"].ToString()!),
+				Descripcion = (string)dr["Descripcion"],
+				Nombre = (string)dr["NombreProducto"]
+			};
+
+			return View(sku);
+		}
+
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public IActionResult Edit(int id, [Bind("Id,Codigo,Nombre,Descripcion")] SKU sku)
+		{
+			if (id != sku.Id)
+			{
+				return NotFound();
+			}
+
+			if (ModelState.IsValid)
+			{
+				DA_SKU daSKU = new DA_SKU(_configuration.GetConnectionString("conexion"));
+
+				daSKU.Modificar(sku);
+
+				return RedirectToAction(nameof(Index));
+			}
+			return View(sku);
+		}
+
+		
+		public IActionResult Delete(int id)
+		{
+			DA_SKU daSKU = new DA_SKU(_configuration.GetConnectionString("conexion"));
+			DataTable dt = daSKU.Obtener(id);
+			if (dt.Rows.Count == 0)
+			{
+				return NotFound();
+			}
+
+			DataRow dr = dt.Rows[0];
+			SKU sku = new SKU
+			{
+				Id = (int)dr["IdSKU"],
+				Codigo = int.Parse(dr["CodProducto"].ToString()!),
+				Descripcion = (string)dr["Descripcion"],
+				Nombre = (string)dr["NombreProducto"]
+			};
+
+			return View(sku);
+		}
+
+		
+		[HttpPost, ActionName("Delete")]
+		[ValidateAntiForgeryToken]
+		public IActionResult DeleteConfirmed(int id)
+		{
+			DA_SKU daSKU = new DA_SKU(_configuration.GetConnectionString("conexion"));
+			daSKU.Eliminar(id);
+
+			return RedirectToAction(nameof(Index));
+		}
+	}
 }
+
--- CRUDSKU/CRUDSKU/Data/DA_SKU.cs
+++ CRUDSKU/CRUDSKU/Data/DA_SKU.cs
@@ -1,41 +1,89 @@
-﻿using System.Data;
+﻿using CRUDSKU.Models;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using System.Data;
 using System.Data.SqlClient;
 
 namespace CRUDSKU.Data
 {
-    public class DA_SKU
-    {
-        private string _conexion;
+	public class DA_SKU
+	{
+		private string _conexion;
 
-        public DA_SKU(string conexion)
-        {
-            _conexion = conexion;
-        }
+		public DA_SKU(string conexion)
+		{
+			_conexion = conexion;
+		}
 
-        public DataTable Listar()
-        {
-            SqlConnection cnn = new(_conexion);
-            SqlCommand cmd = cnn.CreateCommand();
-            cmd.CommandType = System.Data.CommandType.StoredProcedure;
-            cmd.CommandText = "sp_listar";
-            SqlDataAdapter adapter = new SqlDataAdapter(cmd);
-            DataTable dt = new DataTable();
-            adapter.Fill(dt);
+		public DataTable Listar()
+		{
+			SqlConnection cnn = new(_conexion);
+			SqlCommand cmd = cnn.CreateCommand();
+			cmd.CommandType = System.Data.CommandType.StoredProcedure;
+			cmd.CommandText = "sp_listar";
+			SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+			DataTable dt = new DataTable();
+			adapter.Fill(dt);
 
-            return dt;
-        }
+			return dt;
+		}
 
-        public DataTable Obtener()
-        {
-            SqlConnection cnn = new(_conexion);
-            SqlCommand cmd = cnn.CreateCommand();
-            cmd.CommandType = System.Data.CommandType.StoredProcedure;
-            cmd.CommandText = "sp_listar";
-            SqlDataAdapter adapter = new SqlDataAdapter(cmd);
-            DataTable dt = new DataTable();
-            adapter.Fill(dt);
+		public DataTable Obtener(int id)
+		{
+			SqlConnection cnn = new(_conexion);
+			SqlCommand cmd = cnn.CreateCommand();
+			cmd.CommandType = System.Data.CommandType.StoredProcedure;
+			cmd.CommandText = "sp_Obtener";
+			cmd.Parameters.AddWithValue("@IdSKU", id);
+			SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+			DataTable dt = new DataTable();
+			adapter.Fill(dt);
 
-            return dt;
-        }
-    }
+			return dt;
+		}
+
+		public bool Modificar(SKU sku)
+		{
+			using SqlConnection cnn = new(_conexion);
+			using SqlCommand cmd = cnn.CreateCommand();
+			cmd.CommandType = CommandType.StoredProcedure;
+			cmd.CommandText = "sp_Modify";
+			cmd.Parameters.AddWithValue("@IdSKU", sku.Id);
+			cmd.Parameters.AddWithValue("@CodProducto", sku.Codigo);
+			cmd.Parameters.AddWithValue("@NombreProducto", sku.Nombre);
+			cmd.Parameters.AddWithValue("@Descripcion", sku.Descripcion);
+			cnn.Open();
+			int value = cmd.ExecuteNonQuery();
+			cnn.Close();
+			return value == 1;
+		}
+
+		public bool Eliminar(int id)
+		{
+			using SqlConnection cnn = new(_conexion);
+			using SqlCommand cmd = cnn.CreateCommand();
+			cmd.CommandType = CommandType.StoredProcedure;
+			cmd.CommandText = "sp_Eliminar";
+			cmd.Parameters.AddWithValue("@IdSKU", id);
+			cnn.Open();
+			int value = cmd.ExecuteNonQuery();
+			cnn.Close();
+			return value == 1;
+		}
+
+		public bool Insertar(SKU sku)
+		{
+			using SqlConnection cnn = new(_conexion);
+			using SqlCommand cmd = cnn.CreateCommand();
+			cmd.CommandType = CommandType.StoredProcedure;
+			cmd.CommandText = "sp_Guardar";
+			cmd.Parameters.AddWithValue("@CodProducto", sku.Codigo);
+			cmd.Parameters.AddWithValue("@NombreProducto", sku.Nombre);
+			cmd.Parameters.AddWithValue("@Descripcion", sku.Descripcion);
+			cnn.Open();
+			int value = cmd.ExecuteNonQuery();
+			cnn.Close();
+			return value == 1;
+		}
+	}
+
 }
